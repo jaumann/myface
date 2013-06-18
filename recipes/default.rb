7ab88edd8556971002acace55e335251d3f54a68
Jon Aumann <jaumann@millennialmedia.com>
2013-06-18 11:57:21 -0700

    Write out default site

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,17 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package 'apache2'
+ 
+service 'apache2' do
+  action [:enable, :start]
+end
+ 
+template '/var/www/index.html' do
+  owner   'root'
+  group   'root'
+  mode    '0755'
+  source  'index.html.erb'
+end
+
